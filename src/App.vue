dfe746983ef2f64b4da2a81d63350e63862b5de1
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2022-09-26 20:21:00 -0500

    L06-vuedirectivas

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Directivas en VUE.js</h1>
+    <hr/>
+    <h2>v.text</h2>
+    <br>
+    <span v-text="'Hola desde la directiva v-text'" ></span>
+    <br>
+    <span>Hola desde la directiva v-text</span>
+    <br>
+    <span v-text="titulo" ></span>
+    <br>Esta es una transmutacion de texto con una variable prop <br>
+    {{titulo}}
+  </div>
+</template>
+
+  <script>
+    export default{
+      data: () =>({
+        titulo: "Hola desde VUE.JS",
+      }),
+    }
+  </script>
+
+<style scoped>
+  div{
+    text-align: center;
+    margin: 0px 2rem;
+  }
+  
+  
+</style>
